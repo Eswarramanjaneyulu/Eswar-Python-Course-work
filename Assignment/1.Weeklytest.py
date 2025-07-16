ec299cc63e7db335900adac36a3cdfbeb06f455b
Eswar <eswarramanjaneyulu9@gmail.com>
2025-07-16 22:03:35 +0530

    Assignment

---
+++ Assignment/1.Weeklytest.py
@@ -0,0 +1,74 @@
+a= input("enter your data: ").split('-')
+a=a[::-1]
+print('/'.join(a))
+
+date,mounth,year=input("Enter a DOB: ").split()
+print(f"{year}/{mounth}/{date}")
+
+num=int(input(("Enter a number: ")))
+if num%2==0:
+    print("Even Winner")
+else:
+    print("Odd Winner")
+
+data = input("Enter a string: ")
+data=data.replace('a','*')
+data=data.replace('e','*')
+data=data.replace('i','*')
+data=data.replace('o','*')
+data=data.replace('u','*')
+print(data)
+
+a=list(map(float,input("Enter a price: ").split()))
+print(sum(a))
+print(max(a))
+
+a,b=("admin","python123")
+username=input("Enter a username: ")
+password=input("Enter a password: ")
+if username==a and password==b:
+    print("Login Successful")
+else:
+    print("Access Denied")
+
+a=set(map(str,input("Enter a name: ").split()))
+print(a)
+
+data = int(input("Enter number of students: "))
+
+student ={}
+
+for _ in range(data):
+    entry=input().split()
+    name=entry[0]
+    marks=int(entry[1])
+    student[name]=marks
+    
+topper = max(student,key=student.get)
+
+print("First Ranker:",topper)
+
+a=input("Enter a string: ")
+b=[word[::-1] for word in a.split()]
+c=' '.join(b)
+print(c)
+
+a=list(map(int,input("Enter a list: ").split()))
+b=[]
+for item in a:
+    num=int(item)
+    if num !=0:
+        b.append(num)
+print(b)
+
+a=input()
+b={}
+for char in a:
+    if char !=' ':
+        if char in b:
+            b[char]+= 1
+        else:
+            b[char]=1
+print(b)            
+            
+        
---
+++ Assignment/1.assignment.py
@@ -0,0 +1,22 @@
+class Mini_Flipkart:
+    
+ product_id = int(input("Enter Product ID: "))
+ product_name = input("Enter Product Name: ")
+ product_price = float(input("Enter Product Price: "))
+ product_category = input("Enter Product Category: ")
+ available_stock = int(input("Enter Available Stock: "))
+ sold_stock = int(input("Enter Sold Stock: "))
+ discount_percentage = input("Enter Discount Percentage: ")
+ product_features = input("Enter Product Features: ")
+
+ print("\nProduct Details:")
+ print("ID:", product_id)
+ print("Name:", product_name)
+ print("Price: ₹", product_price)
+ print("Category:", product_category)
+ print("Available Stock:", available_stock)
+ print("Sold Stock:", sold_stock)
+ print("Discount:", discount_percentage)
+ print("Features:", product_features)
+
+
