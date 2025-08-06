5eda3fcc787b0b4a26ed0277d617685923309dd1
Eswarramanjaneyulu <eswarramanjaneyulu9@gmail.com>
2025-08-06 13:59:20 +0530

    Classwork

---
+++ Classwork/15.functions.py
@@ -0,0 +1,45 @@
+#Syntax of a Function in Python:
+'''
+def function_add(a,b):
+    return a+b
+a=int(input())
+b=int(input())
+print(function_add(a,b))
+
+def function_name(a):
+    return f"Hello,{a}!"
+a=input()
+print(function_name(a))
+
+#Types of Functions
+#1. Built-in Functions
+def built_function(a,b,c,d):
+    return a,b,c,d
+a=input()
+b=int(input())
+c=list(input())
+d=eval(input())
+print(len(a))
+print(abs(b))
+print(max(c))
+print(sorted(d))
+
+#2. User-Defined Functions
+def function_add(a,b):
+    return a+b
+a=int(input())
+b=int(input())
+print(function_add(a,b))
+'''
+
+    
+
+
+
+
+
+
+
+
+
+
