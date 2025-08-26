9ff77ae05c464278047c94d0389b9a824341508b
Eswarramanjaneyulu <eswarramanjaneyulu9@gmail.com>
2025-08-26 09:59:31 +0530

    numpy

---
+++ numpy/Day1.py
@@ -0,0 +1,49 @@
+
+import numpy as np
+print(np.array([1, 2, 3, 4]))
+
+import numpy as np
+line_space=np.linspace(1,10,9)
+print(line_space)
+
+import numpy as np
+rand_int=np.random.randint(50,100,(10,20))
+print(rand_int)
+
+import numpy as np
+range_arr=np.arange(10,-1,-1)
+print(range_arr)
+
+import numpy as np
+range_arr=np.arange(2,101,2)
+print(range_arr)
+
+import numpy as np
+rand_float=np.random.rand(7,4)
+print(rand_float)
+
+import numpy as np
+fully_array=np.full((5,9),"Eswar")
+print(fully_array)
+
+import numpy as np
+identity=np.eye(4)
+print(identity)
+
+import numpy as np
+rand_norm=np.random.randn(3,3)
+print(rand_norm)
+
+import numpy as np
+rand_choice=np.random.choice(["python","html","css","java","mysql","javascript"],2)
+print(rand_choice)
+
+import numpy as np
+np.random.seed(42)
+rand_arr=np.random.randint(6)
+print(rand_arr)
+
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6]])
+print(arr.shape)
+
